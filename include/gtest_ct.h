2f4057da9a1fe730535ff45cd5336cc8160c789c
michael.okyen <michael.okyen@plexus.com>
2024-11-18 16:53:14 -0500

    Updated code format to match gtest standard.

--- include/gtest_ct.h
+++ include/gtest_ct.h
@@ -1,261 +1,243 @@
-#include "gtest/gtest.h" 
-#include "gtest/gtest-spi.h"
-
+// System headers
 #include <algorithm>
-#include <cmath>
 #include <cctype>
+#include <cmath>
 #include <cstring>
 #include <limits>
 
+// GTest headers
+#include "gtest/gtest-spi.h"
+#include "gtest/gtest.h"
 
-//=================================================
 // C++ VERSION REQUIREMENTS
-//=================================================
 #if __cplusplus < 201103L
-  #error Please compile for C++11 or higher
+#error Please compile for C++11 or higher
 #endif
 
-//=================================================
 // C++11 constexpr string
 // Adapted from Scott Schurr, "New Tools for Class and
 // Library Authors", C++Now 2012
-//=================================================
-class str_const { // constexpr string
-private:
-    const char* const p_;
-    const std::size_t sz_;
-public:
-    template<std::size_t N>
-    constexpr str_const(const char(&a)[N]) : // ctor
-    p_(a), sz_(N-1) {}
+class str_const {  // constexpr string
+ private:
+  const char* const p_;
+  const std::size_t sz_;
 
-    constexpr const char* const getString() const {
-        return p_;
-    }
+ public:
+  template <std::size_t N>
+  constexpr str_const(const char (&a)[N])  // ctor
+      : p_(a), sz_(N - 1) {}
+
+  constexpr const char* const getString() const { return p_; }
 };
 
-//=================================================
 // CT RESULT STORAGE
-//=================================================
 struct result {
 #if __cplusplus < 202002
-    constexpr result(bool assertion, str_const msg) : didTestPass{assertion}, failureMsg{msg} {}
+  constexpr result(bool assertion, str_const msg)
+      : didTestPass{assertion}, failureMsg{msg} {}
 #else
-    consteval result(bool assertion, str_const msg) : didTestPass{assertion}, failureMsg{msg} {}
+  consteval result(bool assertion, str_const msg)
+      : didTestPass{assertion}, failureMsg{msg} {}
 #endif
 
-    const bool didTestPass;
-    str_const failureMsg;
+  const bool didTestPass;
+  str_const failureMsg;
 };
 
-//=================================================
 // STOP ON COMPILE-TIME FAILURE
-//=================================================
 // Define this to fail the build when a CT failure occurs
 // #define STOP_ON_CT_FAIL
 
 #ifdef STOP_ON_CT_FAIL
-    #define ASSERT_ON_BUILD(X) static_assert(X, "gtest_ct failure: "#X);
+#define ASSERT_ON_BUILD(X) static_assert(X, "gtest_ct failure: " #X);
 #else
-    #define ASSERT_ON_BUILD(X) do { } while(0);
+#define ASSERT_ON_BUILD(X) \
+  do {                     \
+  } while (0);
 #endif
 
-//=================================================
 // ASSERTIONS COMMON
-//=================================================
 #define STREAM_FAILURE_MSG "gtest_ct failure: " << x.failureMsg.getString()
 
-//=================================================
 // Utility Function: ULP-based Floating-Point Comparison
-//=================================================
-template<typename T>
+template <typename T>
 constexpr bool almost_equal(T x, T y, int ulp = 4) {
-    static_assert(std::is_floating_point<T>::value, "almost_equal is only for floating-point types.");
-    return std::abs(x - y) <= std::numeric_limits<T>::epsilon() * std::abs(x + y) * ulp
-        || std::abs(x - y) < std::numeric_limits<T>::min();
+  static_assert(std::is_floating_point<T>::value,
+                "almost_equal is only for floating-point types.");
+  return std::abs(x - y) <= std::numeric_limits<T>::epsilon() *
+                               std::abs(x + y) * ulp ||
+         std::abs(x - y) < std::numeric_limits<T>::min();
 }
 
-//=================================================
 // Utility Function: Char to lowercase
-//=================================================
-// Add a helper, because std::tolower is not constexpr
 constexpr char char_to_lower(char c) {
-    return (c >= 'A' && c <= 'Z') ? static_cast<char>(c + ('a' - 'A')) : c;
+  return (c >= 'A' && c <= 'Z') ? static_cast<char>(c + ('a' - 'A')) : c;
 }
 
-//=================================================
 // Utility Function: Case-Insensitive String Comparison
-//=================================================
 constexpr bool strcase_equal(const char* str1, const char* str2) {
-    while (*str1 && *str2) {
-        if (char_to_lower(*str1) != char_to_lower(*str2)) {
-            return false;
-        }
-        ++str1;
-        ++str2;
+  while (*str1 && *str2) {
+    if (char_to_lower(*str1) != char_to_lower(*str2)) {
+      return false;
     }
-    return *str1 == *str2;
+    ++str1;
+    ++str2;
+  }
+  return *str1 == *str2;
 }
 
-//=================================================
 // EXPECTS
-//=================================================
-
-#define CT_EXPECT_TRUE(X) \
-do { \
-    ASSERT_ON_BUILD(X) \
-    constexpr result x{X, #X};\
-    EXPECT_TRUE(x.didTestPass) << STREAM_FAILURE_MSG;\
-} while (0)
+#define CT_EXPECT_TRUE(X)                             \
+  do {                                               \
+    ASSERT_ON_BUILD(X)                               \
+    constexpr result x{X, #X};                       \
+    EXPECT_TRUE(x.didTestPass) << STREAM_FAILURE_MSG; \
+  } while (0)
 
 #define CT_EXPECT_FALSE(X) \
-do { \
-    CT_EXPECT_TRUE(!(X)); \
-} while (0)
+  do {                     \
+    CT_EXPECT_TRUE(!(X));  \
+  } while (0)
 
-#define CT_EXPECT_EQ(X, Y) \
-do { \
-    CT_EXPECT_TRUE(X == Y);\
-} while (0)
+#define CT_EXPECT_EQ(X, Y)    \
+  do {                        \
+    CT_EXPECT_TRUE(X == Y);   \
+  } while (0)
 
-#define CT_EXPECT_NE(X, Y) \
-do { \
-    CT_EXPECT_FALSE(X == Y);\
-} while (0)
+#define CT_EXPECT_NE(X, Y)      \
+  do {                          \
+    CT_EXPECT_FALSE(X == Y);    \
+  } while (0)
 
-#define CT_EXPECT_LT(X, Y) \
-do { \
+#define CT_EXPECT_LT(X, Y)    \
+  do {                        \
     CT_EXPECT_TRUE((X) < (Y)); \
-} while (0)
+  } while (0)
 
-#define CT_EXPECT_LE(X, Y) \
-do { \
+#define CT_EXPECT_LE(X, Y)     \
+  do {                         \
     CT_EXPECT_TRUE((X) <= (Y)); \
-} while (0)
+  } while (0)
 
-#define CT_EXPECT_GT(X, Y) \
-do { \
+#define CT_EXPECT_GT(X, Y)    \
+  do {                        \
     CT_EXPECT_TRUE((X) > (Y)); \
-} while (0)
+  } while (0)
 
-#define CT_EXPECT_GE(X, Y) \
-do { \
+#define CT_EXPECT_GE(X, Y)     \
+  do {                         \
     CT_EXPECT_TRUE((X) >= (Y)); \
-} while (0)
+  } while (0)
 
-#define CT_EXPECT_STREQ(X, Y) \
-do { \
-    CT_EXPECT_TRUE(std::strcmp(X, Y) == 0); \
-} while (0)
+#define CT_EXPECT_STREQ(X, Y)                   \
+  do {                                          \
+    CT_EXPECT_TRUE(std::strcmp(X, Y) == 0);     \
+  } while (0)
 
-#define CT_EXPECT_STRNE(X, Y) \
-do { \
-    CT_EXPECT_FALSE(std::strcmp(X, Y) == 0); \
-} while (0)
+#define CT_EXPECT_STRNE(X, Y)                   \
+  do {                                          \
+    CT_EXPECT_FALSE(std::strcmp(X, Y) == 0);    \
+  } while (0)
 
-#define CT_EXPECT_STRCASEEQ(X, Y) \
-do { \
-    CT_EXPECT_TRUE(strcase_equal(X, Y)); \
-} while (0)
+#define CT_EXPECT_STRCASEEQ(X, Y)           \
+  do {                                      \
+    CT_EXPECT_TRUE(strcase_equal(X, Y));    \
+  } while (0)
 
-#define CT_EXPECT_STRCASENE(X, Y) \
-do { \
-    CT_EXPECT_FALSE(strcase_equal(X, Y)); \
-} while (0)
+#define CT_EXPECT_STRCASENE(X, Y)           \
+  do {                                      \
+    CT_EXPECT_FALSE(strcase_equal(X, Y));   \
+  } while (0)
 
-#define CT_EXPECT_FLOAT_EQ(X, Y) \
-do { \
+#define CT_EXPECT_FLOAT_EQ(X, Y)               \
+  do {                                         \
     CT_EXPECT_TRUE(almost_equal<float>(X, Y)); \
-} while (0)
+  } while (0)
 
-#define CT_EXPECT_DOUBLE_EQ(X, Y) \
-do { \
+#define CT_EXPECT_DOUBLE_EQ(X, Y)               \
+  do {                                          \
     CT_EXPECT_TRUE(almost_equal<double>(X, Y)); \
-} while (0)
+  } while (0)
 
-#define CT_EXPECT_NEAR(X, Y, abs_error) \
-do { \
+#define CT_EXPECT_NEAR(X, Y, abs_error)                \
+  do {                                                 \
     CT_EXPECT_TRUE(std::abs((X) - (Y)) <= (abs_error)); \
-} while (0)
+  } while (0)
 
-//=================================================
 // ASSERTS
-//=================================================
-
-#define CT_ASSERT_TRUE(X) \
-do { \
-    ASSERT_ON_BUILD(X) \
-    constexpr result x{X, #X};\
-    ASSERT_TRUE(x.didTestPass) << STREAM_FAILURE_MSG;\
-} while (0)
+#define CT_ASSERT_TRUE(X)                             \
+  do {                                               \
+    ASSERT_ON_BUILD(X)                               \
+    constexpr result x{X, #X};                       \
+    ASSERT_TRUE(x.didTestPass) << STREAM_FAILURE_MSG; \
+  } while (0)
 
 #define CT_ASSERT_FALSE(X) \
-do { \
-    CT_ASSERT_TRUE(!(X)); \
-} while (0)
+  do {                     \
+    CT_ASSERT_TRUE(!(X));  \
+  } while (0)
 
-#define CT_ASSERT_EQ(X, Y) \
-do { \
-    CT_ASSERT_TRUE(X == Y);\
-} while (0)
+#define CT_ASSERT_EQ(X, Y)    \
+  do {                        \
+    CT_ASSERT_TRUE(X == Y);   \
+  } while (0)
 
-#define CT_ASSERT_NE(X, Y) \
-do { \
-    CT_ASSERT_FALSE(X == Y);\
-} while (0)
+#define CT_ASSERT_NE(X, Y)      \
+  do {                          \
+    CT_ASSERT_FALSE(X == Y);    \
+  } while (0)
 
-#define CT_ASSERT_LT(X, Y) \
-do { \
+#define CT_ASSERT_LT(X, Y)     \
+  do {                         \
     CT_ASSERT_TRUE((X) < (Y)); \
-} while (0)
+  } while (0)
 
-#define CT_ASSERT_LE(X, Y) \
-do { \
+#define CT_ASSERT_LE(X, Y)      \
+  do {                          \
     CT_ASSERT_TRUE((X) <= (Y)); \
-} while (0)
+  } while (0)
 
-#define CT_ASSERT_GT(X, Y) \
-do { \
+#define CT_ASSERT_GT(X, Y)     \
+  do {                         \
     CT_ASSERT_TRUE((X) > (Y)); \
-} while (0)
+  } while (0)
 
-#define CT_ASSERT_GE(X, Y) \
-do { \
+#define CT_ASSERT_GE(X, Y)      \
+  do {                          \
     CT_ASSERT_TRUE((X) >= (Y)); \
-} while (0)
+  } while (0)
 
-#define CT_ASSERT_STREQ(X, Y) \
-do { \
-    CT_ASSERT_TRUE(std::strcmp(X, Y) == 0); \
-} while (0)
+#define CT_ASSERT_STREQ(X, Y)                   \
+  do {                                          \
+    CT_ASSERT_TRUE(std::strcmp(X, Y) == 0);     \
+  } while (0)
 
-#define CT_ASSERT_STRNE(X, Y) \
-do { \
-    CT_ASSERT_FALSE(std::strcmp(X, Y) == 0); \
-} while (0)
+#define CT_ASSERT_STRNE(X, Y)                   \
+  do {                                          \
+    CT_ASSERT_FALSE(std::strcmp(X, Y) == 0);    \
+  } while (0)
 
-#define CT_ASSERT_STRCASEEQ(X, Y) \
-do { \
-    CT_ASSERT_TRUE(strcase_equal(X, Y)); \
-} while (0)
+#define CT_ASSERT_STRCASEEQ(X, Y)           \
+  do {                                      \
+    CT_ASSERT_TRUE(strcase_equal(X, Y));    \
+  } while (0)
 
-#define CT_ASSERT_STRCASENE(X, Y) \
-do { \
-    CT_ASSERT_FALSE(strcase_equal(X, Y)); \
-} while (0)
+#define CT_ASSERT_STRCASENE(X, Y)           \
+  do {                                      \
+    CT_ASSERT_FALSE(strcase_equal(X, Y));   \
+  } while (0)
 
-#define CT_ASSERT_FLOAT_EQ(X, Y) \
-do { \
+#define CT_ASSERT_FLOAT_EQ(X, Y)               \
+  do {                                         \
     CT_ASSERT_TRUE(almost_equal<float>(X, Y)); \
-} while (0)
+  } while (0)
 
-#define CT_ASSERT_DOUBLE_EQ(X, Y) \
-do { \
+#define CT_ASSERT_DOUBLE_EQ(X, Y)               \
+  do {                                          \
     CT_ASSERT_TRUE(almost_equal<double>(X, Y)); \
-} while (0)
+  } while (0)
 
-#define CT_ASSERT_NEAR(X, Y, abs_error) \
-do { \
+#define CT_ASSERT_NEAR(X, Y, abs_error)                \
+  do {                                                 \
     CT_ASSERT_TRUE(std::abs((X) - (Y)) <= (abs_error)); \
-} while (0)
-
+  } while (0)
--- test/passing_tests.cpp
+++ test/passing_tests.cpp
@@ -1,244 +1,217 @@
-#include "gtest_ct.h" 
+#include "gtest_ct.h"
 
-//=================================================
 // Helper Functions and Structures
-//=================================================
-
 constexpr int foo(int x) { return x; }
 
-class TestStruct
-{
-public:
-    constexpr TestStruct(int c, int d) : a{c}, b{d} {}
-    constexpr bool operator==(const TestStruct& rhs) const { return (this->a == rhs.a) && (this->b == rhs.b); }
-    constexpr bool operator<(const TestStruct& rhs) const { return (this->a < rhs.a) || (this->a == rhs.a && this->b < rhs.b); }
+class TestStruct {
+ public:
+  constexpr TestStruct(int c, int d) : a{c}, b{d} {}
+  constexpr bool operator==(const TestStruct& rhs) const {
+    return (this->a == rhs.a) && (this->b == rhs.b);
+  }
+  constexpr bool operator<(const TestStruct& rhs) const {
+    return (this->a < rhs.a) || (this->a == rhs.a && this->b < rhs.b);
+  }
 
-private:
-    int a, b;
+ private:
+  int a, b;
 };
 
-//=================================================
 // EXPECT Tests
-//=================================================
+TEST(CT_EXPECT_TESTS, BASICS) {
+  constexpr bool t{true};
+  CT_EXPECT_TRUE(t);
+  constexpr bool f{false};
+  CT_EXPECT_FALSE(f);
 
-TEST(CT_EXPECT_TESTS, BASICS)
-{
-    constexpr bool t{true};
-    CT_EXPECT_TRUE(t);
-    constexpr bool f{false};
-    CT_EXPECT_FALSE(f);
+  CT_EXPECT_EQ(true, true);
+  CT_EXPECT_NE(true, false);
 
-    CT_EXPECT_EQ(true, true);
-    CT_EXPECT_NE(true, false);
+  CT_EXPECT_EQ(1, 1);
+  CT_EXPECT_NE(1, 2);
 
-    CT_EXPECT_EQ(1, 1);
-    CT_EXPECT_NE(1, 2);
+  constexpr TestStruct A{1, 2};
+  constexpr TestStruct B{1, 2};
+  constexpr TestStruct C{2, 2};
 
-    constexpr TestStruct A{1, 2};
-    constexpr TestStruct B{1, 2};
-    constexpr TestStruct C{2, 2};
-
-    CT_EXPECT_EQ(A, A);
-    CT_EXPECT_EQ(A, B);
-    CT_EXPECT_NE(A, C);
+  CT_EXPECT_EQ(A, A);
+  CT_EXPECT_EQ(A, B);
+  CT_EXPECT_NE(A, C);
 }
 
-TEST(CT_EXPECT_TESTS, RELATIONAL_OPERATORS)
-{
-    constexpr int a = 5;
-    constexpr int b = 10;
+TEST(CT_EXPECT_TESTS, RELATIONAL_OPERATORS) {
+  constexpr int a = 5;
+  constexpr int b = 10;
 
-    CT_EXPECT_LT(a, b);
-    CT_EXPECT_LE(a, b);
-    CT_EXPECT_LE(a, a);
+  CT_EXPECT_LT(a, b);
+  CT_EXPECT_LE(a, b);
+  CT_EXPECT_LE(a, a);
 
-    CT_EXPECT_GT(b, a);
-    CT_EXPECT_GE(b, a);
-    CT_EXPECT_GE(b, b);
+  CT_EXPECT_GT(b, a);
+  CT_EXPECT_GE(b, a);
+  CT_EXPECT_GE(b, b);
 }
 
-TEST(CT_EXPECT_TESTS, FLOATING_POINT_COMPARISONS)
-{
-    constexpr float f1 = 0.1f;
-    constexpr float f2 = f1 + std::numeric_limits<float>::epsilon();
+TEST(CT_EXPECT_TESTS, FLOATING_POINT_COMPARISONS) {
+  constexpr float f1 = 0.1f;
+  constexpr float f2 = f1 + std::numeric_limits<float>::epsilon();
 
-    CT_EXPECT_FLOAT_EQ(f1, f2);
-    CT_EXPECT_DOUBLE_EQ(static_cast<double>(f1), static_cast<double>(f2));
-    CT_EXPECT_NEAR(1.0, 1.001, 0.01);
+  CT_EXPECT_FLOAT_EQ(f1, f2);
+  CT_EXPECT_DOUBLE_EQ(static_cast<double>(f1), static_cast<double>(f2));
+  CT_EXPECT_NEAR(1.0, 1.001, 0.01);
 }
 
-TEST(CT_EXPECT_TESTS, C_STRING_COMPARISONS)
-{
-    constexpr const char* str1 = "hello";
-    constexpr const char* str2 = "hello";
-    constexpr const char* str3 = "world";
-    constexpr const char* str4 = "HELLO";
+TEST(CT_EXPECT_TESTS, C_STRING_COMPARISONS) {
+  constexpr const char* str1 = "hello";
+  constexpr const char* str2 = "hello";
+  constexpr const char* str3 = "world";
+  constexpr const char* str4 = "HELLO";
 
-    CT_EXPECT_STREQ(str1, str2);
-    CT_EXPECT_STRNE(str1, str3);
+  CT_EXPECT_STREQ(str1, str2);
+  CT_EXPECT_STRNE(str1, str3);
 
-    CT_EXPECT_STRCASEEQ(str1, str4);
-    CT_EXPECT_STRCASENE(str1, str3);
+  CT_EXPECT_STRCASEEQ(str1, str4);
+  CT_EXPECT_STRCASENE(str1, str3);
 }
 
-TEST(CT_EXPECT_TESTS, CT_EXPECT_CONTINUES)
-{
-    CT_EXPECT_TRUE(true);
-    EXPECT_NONFATAL_FAILURE({ CT_EXPECT_TRUE(false); }, "");
+TEST(CT_EXPECT_TESTS, CT_EXPECT_CONTINUES) {
+  CT_EXPECT_TRUE(true);
+  EXPECT_NONFATAL_FAILURE({ CT_EXPECT_TRUE(false); }, "");
 }
 
-TEST(CT_EXPECT_TESTS, COMBINED_CT_RT_EXPECT)
-{
-    CT_EXPECT_TRUE(true);
-    EXPECT_NONFATAL_FAILURE({ EXPECT_TRUE(false); }, "");
+TEST(CT_EXPECT_TESTS, COMBINED_CT_RT_EXPECT) {
+  CT_EXPECT_TRUE(true);
+  EXPECT_NONFATAL_FAILURE({ EXPECT_TRUE(false); }, "");
 }
 
-//=================================================
 // ASSERT Tests
-//=================================================
+TEST(CT_ASSERT_TESTS, BASICS) {
+  constexpr bool t{true};
+  CT_ASSERT_TRUE(t);
+  constexpr bool f{false};
+  CT_ASSERT_FALSE(f);
 
-TEST(CT_ASSERT_TESTS, BASICS)
-{
-    constexpr bool t{true};
-    CT_ASSERT_TRUE(t);
-    constexpr bool f{false};
-    CT_ASSERT_FALSE(f);
+  CT_ASSERT_EQ(true, true);
+  CT_ASSERT_NE(true, false);
 
-    CT_ASSERT_EQ(true, true);
-    CT_ASSERT_NE(true, false);
+  CT_ASSERT_EQ(1, 1);
+  CT_ASSERT_NE(1, 2);
 
-    CT_ASSERT_EQ(1, 1);
-    CT_ASSERT_NE(1, 2);
+  constexpr TestStruct A{1, 2};
+  constexpr TestStruct B{1, 2};
+  constexpr TestStruct C{2, 2};
 
-    constexpr TestStruct A{1, 2};
-    constexpr TestStruct B{1, 2};
-    constexpr TestStruct C{2, 2};
-
-    CT_ASSERT_EQ(A, A);
-    CT_ASSERT_EQ(A, B);
-    CT_ASSERT_NE(A, C);
+  CT_ASSERT_EQ(A, A);
+  CT_ASSERT_EQ(A, B);
+  CT_ASSERT_NE(A, C);
 }
 
-TEST(CT_ASSERT_TESTS, RELATIONAL_OPERATORS)
-{
-    constexpr int a = 5;
-    constexpr int b = 10;
+TEST(CT_ASSERT_TESTS, RELATIONAL_OPERATORS) {
+  constexpr int a = 5;
+  constexpr int b = 10;
 
-    CT_ASSERT_LT(a, b);
-    CT_ASSERT_LE(a, b);
-    CT_ASSERT_LE(a, a);
+  CT_ASSERT_LT(a, b);
+  CT_ASSERT_LE(a, b);
+  CT_ASSERT_LE(a, a);
 
-    CT_ASSERT_GT(b, a);
-    CT_ASSERT_GE(b, a);
-    CT_ASSERT_GE(b, b);
+  CT_ASSERT_GT(b, a);
+  CT_ASSERT_GE(b, a);
+  CT_ASSERT_GE(b, b);
 }
 
-TEST(CT_ASSERT_TESTS, FLOATING_POINT_COMPARISONS)
-{
-    constexpr float f1 = 0.1f;
-    constexpr float f2 = f1 + std::numeric_limits<float>::epsilon();
+TEST(CT_ASSERT_TESTS, FLOATING_POINT_COMPARISONS) {
+  constexpr float f1 = 0.1f;
+  constexpr float f2 = f1 + std::numeric_limits<float>::epsilon();
 
-    CT_ASSERT_FLOAT_EQ(f1, f2);
-    CT_ASSERT_DOUBLE_EQ(static_cast<double>(f1), static_cast<double>(f2));
-    CT_ASSERT_NEAR(1.0, 1.001, 0.01);
+  CT_ASSERT_FLOAT_EQ(f1, f2);
+  CT_ASSERT_DOUBLE_EQ(static_cast<double>(f1), static_cast<double>(f2));
+  CT_ASSERT_NEAR(1.0, 1.001, 0.01);
 }
 
-TEST(CT_ASSERT_TESTS, C_STRING_COMPARISONS)
-{
-    constexpr const char* str1 = "hello";
-    constexpr const char* str2 = "hello";
-    constexpr const char* str3 = "world";
-    constexpr const char* str4 = "HELLO";
+TEST(CT_ASSERT_TESTS, C_STRING_COMPARISONS) {
+  constexpr const char* str1 = "hello";
+  constexpr const char* str2 = "hello";
+  constexpr const char* str3 = "world";
+  constexpr const char* str4 = "HELLO";
 
-    CT_ASSERT_STREQ(str1, str2);
-    CT_ASSERT_STRNE(str1, str3);
+  CT_ASSERT_STREQ(str1, str2);
+  CT_ASSERT_STRNE(str1, str3);
 
-    CT_ASSERT_STRCASEEQ(str1, str4);
-    CT_ASSERT_STRCASENE(str1, str3);
+  CT_ASSERT_STRCASEEQ(str1, str4);
+  CT_ASSERT_STRCASENE(str1, str3);
 }
 
-TEST(CT_ASSERT_TESTS, CT_ASSERT_DOESNT_CONTINUE)
-{
-    EXPECT_FATAL_FAILURE({ CT_ASSERT_TRUE(false); }, "");
-    EXPECT_TRUE(false) << "This shouldn't be evaluated";
+TEST(CT_ASSERT_TESTS, CT_ASSERT_DOESNT_CONTINUE) {
+  EXPECT_FATAL_FAILURE({ CT_ASSERT_TRUE(false); }, "");
+  EXPECT_TRUE(false) << "This shouldn't be evaluated";
 }
 
-TEST(CT_ASSERT_TESTS, COMBINED_CT_RT_EXPECT)
-{
-    CT_ASSERT_TRUE(true);
-    EXPECT_NONFATAL_FAILURE({ EXPECT_TRUE(false); }, "");
+TEST(CT_ASSERT_TESTS, COMBINED_CT_RT_EXPECT) {
+  CT_ASSERT_TRUE(true);
+  EXPECT_NONFATAL_FAILURE({ EXPECT_TRUE(false); }, "");
 }
 
-//=================================================
 // EXPECT Failure Tests
-//=================================================
+TEST(CT_EXPECT_FAILURE_TESTS, BASICS) {
+  EXPECT_NONFATAL_FAILURE({ CT_EXPECT_TRUE(false); }, "");
+  EXPECT_NONFATAL_FAILURE({ CT_EXPECT_FALSE(true); }, "");
+  EXPECT_NONFATAL_FAILURE({ CT_EXPECT_EQ(1, 2); }, "");
+  EXPECT_NONFATAL_FAILURE({ CT_EXPECT_NE(1, 1); }, "");
 
-TEST(CT_EXPECT_FAILURE_TESTS, BASICS)
-{
-    EXPECT_NONFATAL_FAILURE({ CT_EXPECT_TRUE(false); }, "");
-    EXPECT_NONFATAL_FAILURE({ CT_EXPECT_FALSE(true); }, "");
-    EXPECT_NONFATAL_FAILURE({ CT_EXPECT_EQ(1, 2); }, "");
-    EXPECT_NONFATAL_FAILURE({ CT_EXPECT_NE(1, 1); }, "");
+  constexpr TestStruct A{1, 2};
+  constexpr TestStruct B{1, 2};
+  constexpr TestStruct C{2, 2};
 
-    constexpr TestStruct A{1, 2};
-    constexpr TestStruct B{1, 2};
-    constexpr TestStruct C{2, 2};
-
-    EXPECT_NONFATAL_FAILURE({ CT_EXPECT_NE(A, A); }, "");
-    EXPECT_NONFATAL_FAILURE({ CT_EXPECT_NE(A, B); }, "");
-    EXPECT_NONFATAL_FAILURE({ CT_EXPECT_EQ(A, C); }, "");
+  EXPECT_NONFATAL_FAILURE({ CT_EXPECT_NE(A, A); }, "");
+  EXPECT_NONFATAL_FAILURE({ CT_EXPECT_NE(A, B); }, "");
+  EXPECT_NONFATAL_FAILURE({ CT_EXPECT_EQ(A, C); }, "");
 }
 
-TEST(CT_EXPECT_FAILURE_TESTS, RELATIONAL_OPERATORS)
-{
-    constexpr int a = 5;
-    constexpr int b = 10;
+TEST(CT_EXPECT_FAILURE_TESTS, RELATIONAL_OPERATORS) {
+  constexpr int a = 5;
+  constexpr int b = 10;
 
-    EXPECT_NONFATAL_FAILURE({ CT_EXPECT_LT(b, a); }, "");
-    EXPECT_NONFATAL_FAILURE({ CT_EXPECT_LE(b, a - 1); }, "");
-    EXPECT_NONFATAL_FAILURE({ CT_EXPECT_GT(a, b); }, "");
-    EXPECT_NONFATAL_FAILURE({ CT_EXPECT_GE(a, b + 1); }, "");
+  EXPECT_NONFATAL_FAILURE({ CT_EXPECT_LT(b, a); }, "");
+  EXPECT_NONFATAL_FAILURE({ CT_EXPECT_LE(b, a - 1); }, "");
+  EXPECT_NONFATAL_FAILURE({ CT_EXPECT_GT(a, b); }, "");
+  EXPECT_NONFATAL_FAILURE({ CT_EXPECT_GE(a, b + 1); }, "");
 }
 
-TEST(CT_EXPECT_FAILURE_TESTS, FLOATING_POINT_COMPARISONS)
-{
-    constexpr float f1 = 0.1f;
-    constexpr float f2 = f1 + 10 * std::numeric_limits<float>::epsilon();
+TEST(CT_EXPECT_FAILURE_TESTS, FLOATING_POINT_COMPARISONS) {
+  constexpr float f1 = 0.1f;
+  constexpr float f2 = f1 + 10 * std::numeric_limits<float>::epsilon();
 
-    EXPECT_NONFATAL_FAILURE({ CT_EXPECT_FLOAT_EQ(f1, f2); }, "");
-    EXPECT_NONFATAL_FAILURE({ CT_EXPECT_DOUBLE_EQ(static_cast<double>(f1), static_cast<double>(f1 * 2)); }, "");
-    EXPECT_NONFATAL_FAILURE({ CT_EXPECT_NEAR(1.0, 1.1, 0.01); }, "");
+  EXPECT_NONFATAL_FAILURE({ CT_EXPECT_FLOAT_EQ(f1, f2); }, "");
+  EXPECT_NONFATAL_FAILURE(
+      { CT_EXPECT_DOUBLE_EQ(static_cast<double>(f1), static_cast<double>(f1 * 2)); },
+      "");
+  EXPECT_NONFATAL_FAILURE({ CT_EXPECT_NEAR(1.0, 1.1, 0.01); }, "");
 }
 
-TEST(CT_EXPECT_FAILURE_TESTS, C_STRING_COMPARISONS)
-{
-    constexpr const char* str1 = "hello";
-    constexpr const char* str2 = "world";
-    constexpr const char* str3 = "HELLO";
+TEST(CT_EXPECT_FAILURE_TESTS, C_STRING_COMPARISONS) {
+  constexpr const char* str1 = "hello";
+  constexpr const char* str2 = "world";
+  constexpr const char* str3 = "HELLO";
 
-    EXPECT_NONFATAL_FAILURE({ CT_EXPECT_STREQ(str1, str2); }, "");
-    EXPECT_NONFATAL_FAILURE({ CT_EXPECT_STRNE(str1, str1); }, "");
-    EXPECT_NONFATAL_FAILURE({ CT_EXPECT_STRCASEEQ(str1, str2); }, "");
-    EXPECT_NONFATAL_FAILURE({ CT_EXPECT_STRCASENE(str1, str3); }, "");
+  EXPECT_NONFATAL_FAILURE({ CT_EXPECT_STREQ(str1, str2); }, "");
+  EXPECT_NONFATAL_FAILURE({ CT_EXPECT_STRNE(str1, str1); }, "");
+  EXPECT_NONFATAL_FAILURE({ CT_EXPECT_STRCASEEQ(str1, str2); }, "");
+  EXPECT_NONFATAL_FAILURE({ CT_EXPECT_STRCASENE(str1, str3); }, "");
 }
 
-//=================================================
 // ASSERT Failure Tests
-//=================================================
+TEST(CT_ASSERT_FAILURE_TESTS, BASICS2) {
+  EXPECT_FATAL_FAILURE({ CT_ASSERT_TRUE(false); }, "");
+  EXPECT_FATAL_FAILURE({ CT_ASSERT_FALSE(true); }, "");
+  EXPECT_FATAL_FAILURE({ CT_ASSERT_EQ(1, 2); }, "");
+  EXPECT_FATAL_FAILURE({ CT_ASSERT_NE(1, 1); }, "");
 
-TEST(CT_ASSERT_FAILURE_TESTS, BASICS2)
-{
-    EXPECT_FATAL_FAILURE({ CT_ASSERT_TRUE(false); }, "");
-    EXPECT_FATAL_FAILURE({ CT_ASSERT_FALSE(true); }, "");
-    EXPECT_FATAL_FAILURE({ CT_ASSERT_EQ(1, 2); }, "");
-    EXPECT_FATAL_FAILURE({ CT_ASSERT_NE(1, 1); }, "");
+  constexpr TestStruct A{1, 2};
+  constexpr TestStruct B{1, 2};
+  constexpr TestStruct C{2, 2};
 
-    constexpr TestStruct A{1, 2};
-    constexpr TestStruct B{1, 2};
-    constexpr TestStruct C{2, 2};
-
-    //TODO not working yet
-    // EXPECT_FATAL_FAILURE({ CT_ASSERT_NE(A, A); }, "");
-    // EXPECT_FATAL_FAILURE({ CT_ASSERT_NE(A, B); }, "");
-    // EXPECT_FATAL_FAILURE({ CT_ASSERT_EQ(A, C); }, "");
+  // TODO(mokyen): not working yet
+  // EXPECT_FATAL_FAILURE({ CT_ASSERT_NE(A, A); }, "");
+  // EXPECT_FATAL_FAILURE({ CT_ASSERT_NE(A, B); }, "");
+  // EXPECT_FATAL_FAILURE({ CT_ASSERT_EQ(A, C); }, "");
 }
