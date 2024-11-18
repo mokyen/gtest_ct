5460253803342f4703665ef6e6f2ef3934d87e19
mokyen <7651443+mokyen@users.noreply.github.com>
2024-11-17 23:20:56 -0500

    Update gtest_ct.h. Added the TODO assertions from the bottom of the file. Need to add tests.

--- include/gtest_ct.h
+++ include/gtest_ct.h
@@ -1,9 +1,12 @@
-#include "gtest/gtest.h"
+#include "gtest/gtest.h" 
 #include "gtest/gtest-spi.h"
-#include <algorithm>
 
-//TODO add usage information
-//https://godbolt.org/z/xhnY87rcM
+#include <algorithm>
+#include <cmath>
+#include <cctype>
+#include <cstring>
+#include <limits>
+
 
 //=================================================
 // C++ VERSION REQUIREMENTS
@@ -35,17 +38,14 @@ public:
 // CT RESULT STORAGE
 //=================================================
 struct result {
-
 #if __cplusplus < 202002
-constexpr result(bool assertion, str_const msg) : didTestPass{assertion}, failureMsg{msg}
-{}
+    constexpr result(bool assertion, str_const msg) : didTestPass{assertion}, failureMsg{msg} {}
 #else
-consteval result(bool assertion, str_const msg) : didTestPass{assertion}, failureMsg{msg}
-{}
+    consteval result(bool assertion, str_const msg) : didTestPass{assertion}, failureMsg{msg} {}
 #endif
 
-const bool didTestPass;
-str_const failureMsg;
+    const bool didTestPass;
+    str_const failureMsg;
 };
 
 //=================================================
@@ -55,9 +55,9 @@ str_const failureMsg;
 // #define STOP_ON_CT_FAIL
 
 #ifdef STOP_ON_CT_FAIL
-#define ASSERT_ON_BUILD(X) static_assert(X, "gtest_ct failure: "#X);
+    #define ASSERT_ON_BUILD(X) static_assert(X, "gtest_ct failure: "#X);
 #else
-#define ASSERT_ON_BUILD(X) do { } while(0);
+    #define ASSERT_ON_BUILD(X) do { } while(0);
 #endif
 
 //=================================================
@@ -66,8 +66,33 @@ str_const failureMsg;
 #define STREAM_FAILURE_MSG "gtest_ct failure: " << x.failureMsg.getString()
 
 //=================================================
+// Utility Function: ULP-based Floating-Point Comparison
+//=================================================
+template<typename T>
+constexpr bool almost_equal(T x, T y, int ulp = 4) {
+    static_assert(std::is_floating_point<T>::value, "almost_equal is only for floating-point types.");
+    return std::abs(x - y) <= std::numeric_limits<T>::epsilon() * std::abs(x + y) * ulp
+        || std::abs(x - y) < std::numeric_limits<T>::min();
+}
+
+//=================================================
+// Utility Function: Case-Insensitive String Comparison
+//=================================================
+constexpr bool strcase_equal(const char* str1, const char* str2) {
+    while (*str1 && *str2) {
+        if (std::tolower(*str1) != std::tolower(*str2)) {
+            return false;
+        }
+        ++str1;
+        ++str2;
+    }
+    return *str1 == *str2;
+}
+
+//=================================================
 // EXPECTS
 //=================================================
+
 #define CT_EXPECT_TRUE(X) \
 do { \
     ASSERT_ON_BUILD(X) \
@@ -90,9 +115,65 @@ do { \
     CT_EXPECT_FALSE(X == Y);\
 } while (0)
 
+#define CT_EXPECT_LT(X, Y) \
+do { \
+    CT_EXPECT_TRUE((X) < (Y)); \
+} while (0)
+
+#define CT_EXPECT_LE(X, Y) \
+do { \
+    CT_EXPECT_TRUE((X) <= (Y)); \
+} while (0)
+
+#define CT_EXPECT_GT(X, Y) \
+do { \
+    CT_EXPECT_TRUE((X) > (Y)); \
+} while (0)
+
+#define CT_EXPECT_GE(X, Y) \
+do { \
+    CT_EXPECT_TRUE((X) >= (Y)); \
+} while (0)
+
+#define CT_EXPECT_STREQ(X, Y) \
+do { \
+    CT_EXPECT_TRUE(std::strcmp(X, Y) == 0); \
+} while (0)
+
+#define CT_EXPECT_STRNE(X, Y) \
+do { \
+    CT_EXPECT_FALSE(std::strcmp(X, Y) == 0); \
+} while (0)
+
+#define CT_EXPECT_STRCASEEQ(X, Y) \
+do { \
+    CT_EXPECT_TRUE(strcase_equal(X, Y)); \
+} while (0)
+
+#define CT_EXPECT_STRCASENE(X, Y) \
+do { \
+    CT_EXPECT_FALSE(strcase_equal(X, Y)); \
+} while (0)
+
+#define CT_EXPECT_FLOAT_EQ(X, Y) \
+do { \
+    CT_EXPECT_TRUE(almost_equal<float>(X, Y)); \
+} while (0)
+
+#define CT_EXPECT_DOUBLE_EQ(X, Y) \
+do { \
+    CT_EXPECT_TRUE(almost_equal<double>(X, Y)); \
+} while (0)
+
+#define CT_EXPECT_NEAR(X, Y, abs_error) \
+do { \
+    CT_EXPECT_TRUE(std::abs((X) - (Y)) <= (abs_error)); \
+} while (0)
+
 //=================================================
 // ASSERTS
 //=================================================
+
 #define CT_ASSERT_TRUE(X) \
 do { \
     ASSERT_ON_BUILD(X) \
@@ -115,16 +196,58 @@ do { \
     CT_ASSERT_FALSE(X == Y);\
 } while (0)
 
+#define CT_ASSERT_LT(X, Y) \
+do { \
+    CT_ASSERT_TRUE((X) < (Y)); \
+} while (0)
 
-// //TODO (plus assert versions)
-// //CT_EXPECT_LT
-// //CT_EXPECT_LE
-// //CT_EXPECT_GT
-// //CT_EXPECT_GE
-// //CT_EXPECT_STREQ?
-// //CT_EXPECT_STRNE?
-// //CT_EXPECT_STRCASEEQ?
-// //CT_EXPECT_STRCASENE?
-// //CT_EXPECT_FLOAT_EQ
-// //CT_EXPECT_DOUBLE_EQ
-// //CT_EXPECT_NEAR
+#define CT_ASSERT_LE(X, Y) \
+do { \
+    CT_ASSERT_TRUE((X) <= (Y)); \
+} while (0)
+
+#define CT_ASSERT_GT(X, Y) \
+do { \
+    CT_ASSERT_TRUE((X) > (Y)); \
+} while (0)
+
+#define CT_ASSERT_GE(X, Y) \
+do { \
+    CT_ASSERT_TRUE((X) >= (Y)); \
+} while (0)
+
+#define CT_ASSERT_STREQ(X, Y) \
+do { \
+    CT_ASSERT_TRUE(std::strcmp(X, Y) == 0); \
+} while (0)
+
+#define CT_ASSERT_STRNE(X, Y) \
+do { \
+    CT_ASSERT_FALSE(std::strcmp(X, Y) == 0); \
+} while (0)
+
+#define CT_ASSERT_STRCASEEQ(X, Y) \
+do { \
+    CT_ASSERT_TRUE(strcase_equal(X, Y)); \
+} while (0)
+
+#define CT_ASSERT_STRCASENE(X, Y) \
+do { \
+    CT_ASSERT_FALSE(strcase_equal(X, Y)); \
+} while (0)
+
+#define CT_ASSERT_FLOAT_EQ(X, Y) \
+do { \
+    CT_ASSERT_TRUE(almost_equal<float>(X, Y)); \
+} while (0)
+
+#define CT_ASSERT_DOUBLE_EQ(X, Y) \
+do { \
+    CT_ASSERT_TRUE(almost_equal<double>(X, Y)); \
+} while (0)
+
+#define CT_ASSERT_NEAR(X, Y, abs_error) \
+do { \
+    CT_ASSERT_TRUE(std::abs((X) - (Y)) <= (abs_error)); \
+} while (0)
+
